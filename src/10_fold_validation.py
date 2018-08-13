77c50d14feb698667a13853ac185306f9f6445ff
Han Zhang <michaelzhhan1990@gmail.com>
2018-08-13 15:27:30 +0800

    add 10 cross validation
    add 10 cross validation

---
+++ src/10_fold_validation.py
@@ -0,0 +1,43 @@
+from sklearn.model_selection import train_test_split
+# read_in data_total as a file, generate 10 sets of different training and testing sets
+
+def k_fold_validation(data_total,label_total):
+    data_all=[]
+    label_all=[]
+
+    for line in open(data_total, encoding='utf-8'):
+        data_all.append(line.strip())
+    for line in open(label_total):
+        label_all.append(line.strip())
+
+    # randomly split data and label into 2 sets, one for train, one for test
+
+    for index in range(0,10):
+        X_train,X_test,y_train,y_test = train_test_split(data_all,label_all,test_size=0.1)
+
+        with open('C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\10_cross_validation\\train\\train_txt.ids.'+str(index), 'w', encoding='utf-8') as f:
+            for ite in X_train:
+                f.write(ite+'\n')
+
+        with open('C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\10_cross_validation\\test\\test_txt.ids.'+str(index), 'w', encoding='utf-8') as f:
+            for ite in X_test:
+                f.write(ite+'\n')
+
+        with open('C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\10_cross_validation\\train\\label.txt.'+str(index), 'w', encoding='utf-8') as f:
+            for ite in y_train:
+                f.write(ite+'\n')
+
+        with open('C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\10_cross_validation\\test\\label_test.txt.'+str(index), 'w', encoding='utf-8') as f:
+            for ite in y_test:
+                f.write(ite+'\n')
+
+data_total='C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\train.txt.ids'
+label_total='C:\\Users\\mycohzhang\\source\\text_classification\\data\\video_info\\label.txt'
+k_fold_validation(data_total,label_total)
+
+
+
+
+
+
+
